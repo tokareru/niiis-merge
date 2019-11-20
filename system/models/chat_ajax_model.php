e848ea205c275d36394fe8621769a7628e392399
DmitryVid <dmytryvid@gmail.com>
2019-11-20 17:40:25 +0300

    add start_ajax

--- system/models/chat_ajax_model.php
+++ system/models/chat_ajax_model.php
@@ -8,59 +8,92 @@ class chat_ajax_model extends model
                 case "print_comment":
                     switch ($_POST["type"]) {
                         case "ALL":
-                            $sql = "SELECT * 
-                                    FROM CHAT 
-                                    WHERE ALL='1'
+                            $sql = "SELECT u.login, c.time, c.comment 
+                                    FROM CHAT c LEFT JOIN
+                                         USERS u on c.cur_user = u.id
+                                    WHERE ALL_CHAT='1'
                                     limit :limit";
                             $q = sys::$PDO->prepare($sql);
                             $q->execute(array("limit" => $_POST["count_messages"]));
-                            return $q->fetchAll();
+                            $Q = $q->fetchAll();
+                            $result;
+                            $result["response"] = 200;
+                            $i = 0;
+                            foreach($Q as $row){
+                                $result[$i]["login"] = $row["login"];
+                                $result[$i]["comment"] = $row["comment"];
+                                $result[$i]["time"] = $row["time"];
+                                $i++;
+                            }
+                            return $result;
                         case "DM":
-                            $sql = "SELECT * 
-                                    FROM CHAT 
-                                    WHERE CUR_USER=:cur_user and USER_CHAT_WITH = :user_chat_with
+                            $sql = "SELECT time, comment
+                                    FROM CHAT
+                                    WHERE CUR_USER = (select id from USERS WHERE LOGIN = :cur_user) and USER_CHAT_WITH = (select id from USERS WHERE LOGIN = :user_chat_with)
                                     limit :limit";
                             $q = sys::$PDO->prepare($sql);
                             $q->execute(array("limit" => $_POST["count_messages"],
                                 "cur_user" => $_POST["current_login"], "user_chat_with" => $_POST["login_user_chat_with"]));
-                            return $q->fetchAll();
+                            $Q = $q->fetchAll();
+                            $result;
+                            $result["response"] = 200;
+                            $i = 0;
+                            foreach($Q as $row){
+                                $result[$i]["login"] = $_POST["current_login"];
+                                $result[$i]["comment"] = $row["comment"];
+                                $result[$i]["time"] = $row["time"];
+                                $i++;
+                            }
+                            return $result;
+                        default:
+                            return array("response"=>"TYPE '".$_POST["type"]."' NOT FOUND");
                     }
                     break;
                 case "add_comment":
                     switch ($_POST["type"]) {
                         case "ALL":
-                            $sql = "INSERT INTO CHAT(ALL,TIME,CUR_USER,COMMENT)
-                                    VALUES(:all,:time,(select USER_ID from USERS WHERE USER_NAME=:cur_user),:comment)";
+                            $sql = "INSERT INTO CHAT(ALL_CHAT,CUR_USER,COMMENT)
+                                    VALUES(:all,(select ID from USERS WHERE LOGIN=:cur_user),:comment)";
                             $q = sys::$PDO->prepare($sql);
-                            $q->execute(array("all"=>1, "time"=>$_POST["time"],
-                                "cur_user"=>$_POST["current_login"], "comment" => $_POST["comment"]));
+                            $q->execute(array("all"=>'1',"cur_user"=>$_POST["current_login"], "comment" => $_POST["comment"]));
                             return array("response"=>200);
                         case "DM":
                              case "ALL":
-                            $sql = "INSERT INTO CHAT(TIME,USER_CHAT_WITH,CUR_USER,COMMENT)
-                                    VALUES(:time,(select USER_ID from USERS WHERE USER_NAME=:user_chat_with),(select USER_ID from CHAT_USERS WHERE USER_NAME=:cur_user),:comment)";
-                            $q = sys::$PDO->prepare($sql);
-                            $q->execute(array("user_chat_with"=>$_POST["login_user_chat_with"], "time"=>$_POST["time"],
-                                "cur_user"=>$_POST["current_login"], "comment" => $_POST["comment"]));
-                            return array("response"=>200);
+                            if($_POST["current_login"] == $_POST["login_user_chat_with"]){
+                                $sql = "INSERT INTO CHAT(USER_CHAT_WITH,CUR_USER,COMMENT)
+                                        VALUES((select ID from USERS WHERE LOGIN=:user_chat_with),(select ID from USERS WHERE LOGIN=:cur_user),:comment)";
+                                $q = sys::$PDO->prepare($sql);
+                                $q->execute(array("user_chat_with"=>$_POST["login_user_chat_with"],
+                                    "cur_user"=>$_POST["current_login"], "comment" => $_POST["comment"]));
+                                return array("response"=>200);
+                            }else{
+                                return array("response"=>"You can't sent messange by self");
+                            }
+                        default:
+                            return array("response"=>"TYPE '".$_POST["type"]."' NOT FOUND");
                     }
                     break;
                 case "count_comments":
                     switch ($_POST["type"]) {
                         case "ALL":
                             $sql = "SELECT COUNT(*) as COUNT
-                                    FROM CHAT";
+                                    FROM CHAT
+                                    WHERE ALL_CHAT = '1'";
                             $q = sys::$PDO->prepare($sql);
                             $q->execute();
-                            return $q->fetchAll();
+                            $Q = $q->fetchAll();
+                            return array("count"=>$Q[0]["count"]);
                         case "DM":
                             $sql = "SELECT COUNT(*) as COUNT
                                     FROM CHAT
-                                    WHERE CUR_USER=:cur_user and USER_CHAT_WITH = :user_chat_with";
+                                    WHERE CUR_USER = (select id from USERS WHERE LOGIN = :cur_user) and USER_CHAT_WITH = (select id from USERS WHERE LOGIN = :user_chat_with)";
                             $q = sys::$PDO->prepare($sql);
                             $q->execute(array("cur_user" => $_POST["current_login"], 
                                 "user_chat_with" => $_POST["login_user_chat_with"]));
-                            return $q->fetchAll();
+                            $Q = $q->fetchAll();
+                            return array("count"=>$Q[0]["count"]);
+                        default:
+                            return array("response"=>"TYPE '".$_POST["type"]."' NOT FOUND");
                     }
                     break;
                 case "login_users":
@@ -83,6 +116,8 @@ class chat_ajax_model extends model
                     $q->execute();
                     $Q = $q->fetchAll();
                     return array("count"=>$Q[0]["count"]);
+                default:
+                    return array("response"=>"POST METHOD '".$_POST["function"]."' NOT FOUND");
             }
         }
         else {
