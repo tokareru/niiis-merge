8ad67904f4637865138bb09751cf3e37be69497d
DmitryVid <dmytryvid@gmail.com>
2019-11-21 12:25:37 +0300

    test spec_table_save

--- system/models/spec_table_ajax_model.php
+++ system/models/spec_table_ajax_model.php
@@ -1,30 +1,64 @@
 <?php
-class spec_table_ajax_model extends model 
-{
-  public function get_data() 
-  {
-    $sql = "SELECT * FROM SPEC_TABLE";
-    $q = sys::$PDO->prepare($sql);
-    $q->execute();
-    $Q = $q->fetchAll();
-    $result["thead"] = array(array('text'=>"Поз.",'readonly'=>true),array('text'=>"Обозначение",'readonly'=>true),array('text'=>"Наименование",'readonly'=>true),array('text'=>"Кол.",'readonly'=>true));
-    $i = 0;
-    foreach($Q as $row){
-        $j = 0;
-        $is_readonly = $row["is_read_only"];
-        for($k=0; $k < count($row); $k+=2){
-            
-            if($row["is_read_only"] != $row[$j]){
-                $readonly = true;
-//                if($is_readonly[$j++] == 'f'){
-//                    $readonly = false;
-//                }  
-                $result['tbody'][$i]["row"][$j] = array('text' => $row[$j],'readonly'=>$readonly);
-                $j++;
+
+class spec_table_ajax_model extends model {
+
+    public function get_data() {
+        $sql = "SELECT * FROM SPEC_TABLE";
+        $q = sys::$PDO->prepare($sql);
+        $q->execute();
+        $Q = $q->fetchAll();
+        $result["thead"] = array(array('text' => "Поз.", 'readonly' => true), array('text' => "Обозначение", 'readonly' => true), array('text' => "Наименование", 'readonly' => true), array('text' => "Кол.", 'readonly' => true));
+        $i = 0;
+
+        foreach ($Q as $row) {
+            $j = 0;
+            $is_readonly = $row["is_read_only"];
+            for ($k = 0; $k < count($row) - 2; $k += 2) {
+
+                if ($row["is_read_only"] != $row[$j]) {
+                    $readonly = true;
+                if($is_readonly[$j] == 'f'){
+                    $readonly = false;
+                }  
+                    $result['tbody'][$i]["row"][$j] = array('text' => $row[$j], 'readonly' => $readonly);
+
+                    $j++;
+                }
             }
+            $i++;
         }
-        $i++;
+        return $result;
     }
-    return $result;
-  }
+
+    function save() {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//            $sql = "DELETE * FROM SPEC_TABLE";
+//                $q = sys::$PDO->prepare($sql);
+//                $q->execute();
+            
+//              return json_decode($Q);
+            return $_POST["tbody"];
+//            foreach($Q as $row){
+//                
+//                $readonly_str = "";
+//                for($i = 0; $i<4;$i++){
+//                    if($row[$i++]["readonly"]){
+//                        $readonly_str .= "t"; 
+//                    }else{
+//                        $readonly_str .= "f";
+//                    }
+//                }
+//                print_r($row);
+////                $sql = "INSERT SPEC_TABLE(POSITION,NAME_SHORT,NAME_LONG,COUNT,IS_READ_ONLY)
+////                        VALUES(:position, :name_short, :name_long, :count, :readonly)";
+////                $q = sys::$PDO->prepare($sql);
+////                $i = 0;
+////                $q->execute(array("position"=>$row[$i++]["text"],"name_short"=>$row[$i++]["text"],"name_long"=>$row[$i++]["text"],
+////                    "count"=>$row[$i++]["text"],"readonly"=>$readonly_str));
+//            }
+        } else {
+            return array("response" => "NOT FOUND POST REQUEST");
+        }
+    }
+
 }
