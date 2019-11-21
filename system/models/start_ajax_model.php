ba435b8d51f3e83edc16624303c5117fdaf10bbe
DmitryVid <dmytryvid@gmail.com>
2019-11-21 21:55:21 +0300

    test request to site name

--- system/models/start_ajax_model.php
+++ system/models/start_ajax_model.php
@@ -4,7 +4,8 @@ class start_ajax_model extends model
   public function get_data() 
   {
     if($_SERVER["REQUEST_METHOD"]=="GET"){
-            return array("login"=>sys::user_login(),"role"=>$_SESSION['niiis']['role'], "round"=>$_SESSION['niiis']['round']);
+            return array("login"=>sys::user_login(),"role"=>$_SESSION['niiis']['role'], "round"=>$_SESSION['niiis']['round'],
+                "server_name"=>trim($_SERVER['SERVER_NAME'],'/')    );
         }
         else {
             return array("response"=>"NOT FOUND POST REQUEST");
