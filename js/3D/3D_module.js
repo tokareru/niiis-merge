45be5a31b4c850e6aa764af01a6be80402784468
Ruslan <ruslan.da2011@gmail.com>
2019-11-26 16:48:40 +0300

    3d fix+

--- js/3D/3D_module.js
+++ js/3D/3D_module.js
@@ -121,7 +121,7 @@ export function init3dField() {
 
     }
 	
-	function loadSTL (src, color, arrmesh, pos = { x:0, y:-0.05, z:0 }, rot = { x:-Math.PI / 2, y:0, z:0 }, scale = { x:0.5, y:0.5, z:0.5 })
+	function loadSTL (src, color, arrmesh, pos = { x:0, y:-0.315, z:0 }, rot = { x:-Math.PI / 2, y:0, z:0 }, scale = { x:0.5, y:0.5, z:0.5 })
 	{
 		
 		loader.load(src, function (geometry) {
