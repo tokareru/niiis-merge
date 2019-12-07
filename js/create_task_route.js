3e1c2905eb1d11e5a14f1511c48f50bd56cdf0b0
Andrey_Tsibrov <andrtsibrov@mail.ru>
2019-12-08 01:14:40 +0300

    add ajax request for "create task route"

--- js/create_task_route.js
+++ js/create_task_route.js
@@ -1,5 +1,7 @@
 function initCreate_task_route() {
-    addRows(getLoginNames().length);
+
+    addRows(getLoginNames().length, false, true);
+
     $('.slider_button_create').on('click', function () {
         STDLibClick($('.slider_button_create'), $('.main_all_create_task'), 25);
     });
@@ -21,23 +23,45 @@ function initCreate_task_route() {
     });
 
     $('.create_task_route_button').on('click', function () {
-        serializeCreateTaskRoute();
+        let arr = serializeCreateTaskRoute();
+        if (arr.length !== 0) {
+            createTaskRouteToServer(arr);
+        }
     });
 }
 
-function addRows(count_rows) {
+function addRows(count_rows, addInputs, addRolesNames) {
+    let UsersRoles = getLoginNames('role');
+    let UsersNames = getLoginNames('name');
+
 
     let $tbody = $('.table_create_task_route').find('tbody');
     for (let i = 0; i < count_rows; i++) {
+        let input_div_roles = '';
+        let input_div_names = '';
+        let role = '';
+        let name = '';
+        if (addRolesNames) {
+            role = UsersRoles[i];
+            name = UsersNames[i];
+        }
+        if (addInputs) {
+            input_div_roles = '<div class="ctr_div_active">' + role + '</div>' +
+                '<input type="text" class="ctr_input_hidden" value="' + role + '">';
+            input_div_names = '<div class="ctr_div_active">' + name + '</div>' +
+                '<input type="text" class="ctr_input_hidden" value="' + name + '">';
+        } else {
+            input_div_roles = role;
+            input_div_names = name;
+        }
+
         let $td = '<td style="text-align: center">' +
             '<input type="checkbox"' +
             '>' +
             '</td>' +
-            '<td class="ctr_cell"><div class="ctr_div_active"></div>' +
-            '<input type="text" class="ctr_input_hidden">' +
+            '<td class="ctr_cell">' + input_div_roles +
             '</td>' +
-            '<td class="ctr_cell"><div class="ctr_div_active"></div>' +
-            '<input type="text" class="ctr_input_hidden">' +
+            '<td class="ctr_cell">' + input_div_names +
             '</td>' +
             '<td>' +
             '<select class="form-control form-control-sm">' +
@@ -58,20 +82,32 @@ function serializeCreateTaskRoute() {
     for (let i = 0; i < count_rows; i++) {
         let tempData = [];
         if ($table.find('tbody').find('tr').eq(i).find('td').eq(0).find('input:checkbox:checked').length > 0) {
-            tempData.push(true);
+            for (let j = 1; j < count_cols; j++) {
+                if (j !== count_cols - 1)
+                    tempData.push($table.find('tbody').find('tr').eq(i).find('td').eq(j).text());
+                else {
+                    tempData.push($table.find('tbody').find('tr').eq(i).find('td')
+                        .eq(j).find('option:selected').text());
+                }
+            }
+            data.push(tempData);
         }
-        else tempData.push(false);
-        for (let j = 1; j < count_cols; j++) {
-            if(j !== count_cols - 1 )
-                tempData.push($table.find('tbody').find('tr').eq(i).find('td').eq(j).text());
-            else {
-                tempData.push($table.find('tbody').find('tr').eq(i).find('td')
-                    .eq(j).find('option:selected').text());
+    }
+    return data;
+}
+
+function createTaskRouteToServer(dataToAjax) {
+    $.ajax(
+        {
+            url: '',
+            type: 'POST',
+            data: {data: dataToAjax},
+            success: function (data) {
+
+            },
+            error: function () {
+
             }
         }
-        data.push(tempData);
-
-    }
-    console.log(data);
-
+    )
 }
