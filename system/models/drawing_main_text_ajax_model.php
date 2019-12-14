099f0f10a3e45ea696c97cf464a241e661d5eaf3
DmitryVid <dmytryvid@gmail.com>
2019-12-14 13:25:34 +0300

    add saves main text

--- system/models/drawing_main_text_ajax_model.php
+++ system/models/drawing_main_text_ajax_model.php
@@ -7,17 +7,39 @@ class drawing_main_text_ajax_model extends model {
     }
 
     function save() {
-        $i = 1;
-        $data = [];
-        $sql = "UPDATE DRAWING_MAIN_TEXT SET";
-        foreach ($_POST["body"] as $row) {
+        $sql = "SELECT * FROM DRAWING_MAIN_TEXT";
+        $q = sys::$PDO->prepare($sql);
+        $q->execute($data);
+        $Q = $q->fetchAll();
+        if($Q){
+            $sql = "UPDATE DRAWING_MAIN_TEXT SET";
+            foreach ($_POST["body"] as $row) {
             $sql .= " field$i = :field$i";
             $data["field$i"] = $row;
-        }
+            $i++;
+            }
         
         $q = sys::$PDO->prepare($sql);
         $q->execute($data);
         $Q = $q->fetchAll();
+        
+            
+        }else{
+        $i = 1;
+        $data = [];
+        $sql = "INSERT INTO DRAWING_MAIN_TEXT(";
+        $str = "VALUES (";
+        foreach ($_POST["body"] as $row) {
+            $sql .= " field$i";
+            $str.=" :field$i";
+            $data["field$i"] = $row;
+            $i++;
+        }
+        $sql.=") ".$str.");";
+        $q = sys::$PDO->prepare($sql);
+        $q->execute($data);
+        $Q = $q->fetchAll();
+        }
     }
 
 }
