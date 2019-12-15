92a46927fa9e963a313c699493556c964540ceb8
DmitryVid <dmytryvid@gmail.com>
2019-12-15 21:43:06 +0300

    add save_product

--- system/models/spec_table_ajax_model.php
+++ system/models/spec_table_ajax_model.php
@@ -29,7 +29,20 @@ class spec_table_ajax_model extends model {
         }
         return $result;
     }
-
+    function save_product(){
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $sql = "INSERT PRODUCT(MODEL_NAME, PATH_3D, DESCRIPTION, USER_ID, TYPE_ID)
+                    VALUES(:model_name, :path_3d, :description, (SELECT ID FROM USERS WHERE LOGIN = :login), 
+                    (SELECT TYPE_ID FROM PRODUCT_TYPE WHERE TYPE=:type))
+                    ";
+            $q = sys::$PDO->prepare($sql);
+            $q->execute(array("model_name" => $_POST["model_name"], "path_3d"=>$path_3d, 
+                "description"=>$description, "login"=>$login, "type"=>$type));
+        } else {
+            return array("response" => "NOT FOUND POST REQUEST");
+        }
+    }
+    
     function save() {
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $sql = "UPDATE SPEC_TABLE SET ACTIVE_SIGN = '0'";
@@ -52,12 +65,12 @@ class spec_table_ajax_model extends model {
                     }
                 }
                 
-                $sql = "INSERT INTO SPEC_TABLE(POSITION,NAME_SHORT,NAME_LONG,COUNT,IS_READ_ONLY, ROUND, PDM_ID, USER_ID)
-                        VALUES(:position, :name_short, :name_long, :count, :readonly, :round, 1, (SELECT ID FROM USERS WHERE login=:login))";
+                $sql = "INSERT INTO SPEC_TABLE(POSITION,NAME_SHORT,NAME_LONG,COUNT,IS_READ_ONLY, ROUND, PRODUCT_ID, USER_ID)
+                        VALUES(:position, :name_short, :name_long, :count, :readonly, :round, :product_id, (SELECT ID FROM USERS WHERE login=:login))";
                 $q = sys::$PDO->prepare($sql);
                 $i = 0;
                 $q->execute(array("position" => $row["row"][$i++]["text"], "name_short" => $row["row"][$i++]["text"], "name_long" => $row["row"][$i++]["text"],
-                    "count" => $row["row"][$i++]["text"], "readonly" => $readonly_str,'round'=> $round, 'login'=> $_POST['login']));
+                    "count" => $row["row"][$i++]["text"], "readonly" => $readonly_str,'round'=> $round, 'product_id'=>$product_id, 'login'=> $_POST['login']));
             }
             return array("response" => 200);
         } else {
