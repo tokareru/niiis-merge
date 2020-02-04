9add2cb0da890f805363b4c3758174c657af6bcf
Sergey Tsibrov <cybrovsergey@gmail.com>
2020-02-04 23:46:35 +0300

    change tech guide

--- js/technologist_guide.js
+++ js/technologist_guide.js
@@ -8,49 +8,92 @@ function setTechnologistGuide(json, add_data) {
     field.append("<div id='technologist_guide_accordion'></div>");
     let field_accord = field.find("#technologist_guide_accordion");
     if (field_accord.children().length) return;
-    json.forEach(function (types, index) {
-        field_accord.append("<p class='tg_header'>" + types.name + "</p><div class='tg_div accordion' role=\"tablist\" aria-multiselectable=\"true\" id='" + types.type + "'></div>");
-       /* types.elements.forEach(function (elem) {
-            field.find(".tg_div").last()
-                .append("<div class='card'>" +
-                    "<div class='card-header' id='card_header_" + index + "' >" +
-                    "<h6>" +
-                        "<button type='button' data-toggle='collaps' data-target='#colapsible_" + index + "' class='tg_elem_header' " +
-                            "aria-expanded='true' aria-controls='colapsible_" + index + "'>"
-                            + elem.name +
-                        "</button>" +
-                        "</h6>"
-                    + "</div>" +
-                    "<div id='colapsible_" + index + "' class='tg_elem_div collapse' aria-labelledby='card_header_" + index + "' data-parent='#" + types.type +"'>"
-                        + "<div class='card-body'>"
-                            + elem.text +
-                        "</div>" +
-                    "</div>" +
-                   "</div>")
-        });*/
 
-        types.elements.forEach(function (elem) {
-            field.find(".tg_div").last()
-                .append("<p class='tg_elem_header'>" + elem.name + "</p><div class='tg_elem_div'>" + elem.text + "</div>")
-        });
+    let techs = '';
 
-        field.find('div .tg_elem_div').each(function () {
-            $(this).draggable({
-                helper: 'clone'
-            });
-        });
-
-        field.find(".tg_div").last().accordion({
-            heightStyle: "content",
-            animate: 50,
-            collapsible: true,
-            active: false
-        });
+    json.forEach(function (child) {
+        techs += createTechGuideNodes(child);
     })
-    field_accord.accordion({
-        heightStyle: "content",
-        animate: 200,
-        collapsible: true,
-        active: false
+
+    $("#technologist_guide_accordion").append(
+        "<ul class='col-12 pl-0'>" +
+            techs +
+        "</ul>"
+    )
+
+    let toggler = document.getElementById("technologist_guide_accordion").getElementsByClassName("caret");
+
+    for (let i = 0; i < toggler.length; i++) {
+        toggler[i].addEventListener("click", function () {
+            this.parentElement.querySelector(".nested").classList.toggle("active");
+            this.classList.toggle("caret-down");
+        });
+    }
+
+    $("#technologist_guide_accordion").find(".detailChildren").trigger("click");
+}
+
+
+function createTechGuideNodes(tech) {
+    let node = '';
+    let isDisabled = "disabled";
+    console.log(tech);
+
+    let inp = '';
+
+    tech.children.forEach(function (child, i) {
+        console.log(child)
+        let equip = '';
+        child.equipment.forEach(function (eq) {
+            equip +=
+                "<li class='lastChild instruments_list_li'>" +
+                "<span>" + eq.name + "</span>" +
+                "</li>";
+        });
+
+        let instr = '';
+        child.instruments.forEach(function (ins) {
+            instr +=
+                "<li class='lastChild instruments_list_li'>" +
+                    "<span>" + ins.name + "</span>" +
+                "</li>"
+        });
+
+        inp +=
+            "<il>" +
+                "<span class='caret'>" + (i + 1).toString() + "." + child.name + "</span>" +
+                "<ul class='nested pl-2'>" +
+                    "<li>" +
+                        "<span class='caret'>Название</span>" +
+                        "<ul class='nested pl-2'>" +
+                            "<li class='lastChild instruments_list_li'>" +
+                                "<span class=''>" + child.name + "</span>" +
+                            "</li>" +
+                        "</ul>" +
+                    "</li>" +
+                    "<li>" +
+                        "<span class='caret'>Оборудование</span>" +
+                        "<ul class='nested pl-2'>" +
+                            equip +
+                        "</ul>" +
+                    "</li>" +
+                    "<li>" +
+                        "<span class='caret'>Инструменты</span>" +
+                        "<ul class='nested pl-2'>" +
+                            instr +
+                        "</ul>" +
+                    "</li>" +
+                "</ul>" +
+            "</il>";
     });
+
+    node =
+        "<li>" +
+            "<span class='caret detailChildren'>" + tech.name + "</span>" +
+            "<ul class='nested pl-2'>" +
+                inp +
+            "</ul>" +
+        "</li>";
+
+    return node;
 }
