01f1d8e96de8d299baa0bd260921999a91eeebba
Sergey Tsibrov <cybrovsergey@gmail.com>
2020-03-09 18:49:48 +0300

    fix scheme and 3D styles

--- system/views/scheme_field_view.php
+++ system/views/scheme_field_view.php
@@ -14,7 +14,7 @@
 <body>
 <button class="btn-block btn btn-custom" id="ready">Готово</button>
 <div id='field3DAll'>
-    <div id="field3D">
+    <div id="field3D" class="mx-auto">
         <div id="scheme1">
             <div id="hidedraw"></div>
             <canvas id="drawcanv"></canvas>
