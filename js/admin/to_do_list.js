5559ea85795b6f93096ec34545b613ba3f12093b
ruslan.da2011@gmail.com <8960190Rus>
2020-05-08 15:15:00 +0300

    to do list

--- js/admin/to_do_list.js
+++ js/admin/to_do_list.js
@@ -3,10 +3,103 @@ $(function () {
         url: 'get_change_users',
         type: 'GET',
         success: function (data) {
+            addlogins(data);
             console.log(data)
         },
         error: function (data) {
             console.log('error');
         }
     });
-})
+
+    $( "#addTask" ).on( "click", function() {
+        createTask();
+    });
+
+    $( "#saveUserTasks" ).on( "click", function() {
+        alert();
+    });
+
+    $("#change_user_login").change(function() {
+        checkOnchange();
+    });
+
+    $("#change_user_round").change(function() {
+        checkOnchange();
+    });
+
+    $(".taskselect").change(function() {
+
+    })
+
+});
+
+function addlogins(d)
+{
+    for (let i=0; i<d.users.length; i++)
+    {
+        $("#change_user_login").append("<option value="+(i+1)+">"+d.users[i][0]+"</option>")
+    }
+}
+
+function createTask()
+{
+    let t = getTasks();
+    let count = $(".tasknumber").length+1;
+    let code = `<span class="tasknumber">Задача №`+count+`</span>
+	<div class="task">
+	<div>
+	Описание
+	<textarea class="form-control form-control-col-1">test</textarea>
+	</div>
+	<div class="taskselect">
+	Выберите действие
+	<select class="form-control" id="task`+count+`">
+	<option></option>
+	</select>
+	</div>
+	</div>`;
+    $("#contentblock").append(code);
+
+    for (let i=0; i<t.tasks.length; i++)
+    {
+        $("#task"+count).append("<option value="+(i+1)+">"+t.tasks[i].text+"</option>");
+    }
+
+}
+
+function checkOnchange()
+{
+    if ($("#change_user_login").prop('selectedIndex') !== 0 && $("#change_user_round").prop('selectedIndex') !== 0)
+    {
+        let t = getTasks();
+        for (let i=0; i<t.tasks.length; i++)
+        {
+            //$("#task"+$(".tasknumber").length+1).append("<option value="+(i+1)+">"+d.users[i][0]+"</option>")
+            // ajax получить задачи для каждого логина и раунда
+        }
+        $("#actionButtons").show();
+    }
+    else
+    {
+        $("#actionButtons").hide();
+    }
+}
+
+function getTasks()
+{
+    var h;
+    $.ajax({
+        type: 'GET',
+        async : false,
+        url: '../json/to_do_list_config.json',
+        success: function (json) {
+            h = json;
+        }
+    });
+    return h;
+}
+
+
+
+
+
--- system/views/admin/to_do_list.php
+++ system/views/admin/to_do_list.php
@@ -7,6 +7,35 @@
     sys::inc_no_cache('javascript', 'js/libraries/jquery-ui.js');
 
     ?>
+<link href="../../css/to_do_list.css" rel="stylesheet" type="text/css">
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+<h1 class="h2">Задачи пользователей</h1>
+</div>
+
+<div id="contentblock">
+<div class="userdatainput">
+<div style="min-width: 104px;">Выберите логин пользователя
+<select class="form-control col-12" id="change_user_login">
+<option></option>
+
+</select>
+</div>
+
+<div style="margin-left:10px; margin-right: 10px;">Выберите раунд
+<select class="form-control col-12" id="change_user_round">
+<option></option>
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+</select>
+</div>
+
+<div id="actionButtons" style="display: none;">
+<button id="addTask" class="btn btn-outline-primary">Добавить задачу</button>
+<button id="saveUserTasks" class="btn btn-primary" disabled>Сохранить</button>
+</div>
+</div>
+
+
 
 </div>
