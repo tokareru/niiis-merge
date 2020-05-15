42d14cb80ae93a90697920b5b829e56a6a44e4a2
makss <makss@DESKTOP-LK19C8L>
2020-05-15 22:24:14 +0300

    test

--- system/base/route.php
+++ system/base/route.php
@@ -1,147 +1,141 @@
 <?php
-class Route
-{
-  static $path;
-  static $fields;
-	static function start()
-	{ 
-    global $CONF;
-		// контроллер и действие по умолчанию
-    
-    $controller_name = 'default';
-    
-    $action_name = 'index';
-   
-		
-    $aUrl=parse_url(trim($_SERVER['REQUEST_URI'],'/\\'));
-    $path = explode('/', $aUrl['path']);
-    
-    //array_shift($path); // удаляем первый элемент - год
-    for($i=0;$i<conf::$SITE_SUBDIR_INDEX;$i++){
-      array_shift($path); // удаляем кол-вол вложенных директорий
-    }
-    
-    self::$path=$path;
-    
-    if(!isset($path[0]) || !$path[0]){
-        $modul = 'default';
-    }else{
-        $modul = $path[0];
-    }
-    if(sys::is_autorised()){
-        if($_SESSION["niiis"]["role"] === "administrator"){
-            if($modul =='default'){
-                $modul ='admin_cab';
-            }
+
+class Route {
+
+    static $path;
+    static $fields;
+
+    static function start() {
+        global $CONF;
+        // контроллер и действие по умолчанию
+        $controller_name = 'default';
+        $action_name = 'index';
+
+        $aUrl = parse_url(trim($_SERVER['REQUEST_URI'], '/\\'));
+        $path = explode('/', $aUrl['path']);
+
+        for ($i = 0; $i < conf::$SITE_SUBDIR_INDEX; $i++) {
+            array_shift($path); // удаляем кол-вол вложенных директорий
         }
-      require_once conf::$ROOT.'system/etc/functions.php';
-      
-      if($modul=='ajax'){
-        $controller_file = 'ajax_controller.php';
-        if(file_exists("system/controllers/".$controller_file)){
-          include "system/controllers/".$controller_file;
-          $controller_name='ajax_controller';
-          if (class_exists($controller_name)){
-            $controller = new $controller_name;
-            $action = $path[1];
-                if (method_exists($controller, $action))
-                {
-                  // вызываем действие контроллера
-                  $param='';
-                  if (isset($param))
+
+        self::$path = $path;
+
+        if (!isset($path[0]) || !$path[0]) {
+            $modul = 'default';
+        } else {
+            $modul = $path[0];
+        }
+        if (sys::is_autorised()) {
+            if ($_SESSION["niiis"]["role"] === "administrator") {
+                if ($modul == 'default') {
+                    $modul = 'admin_cab';
+                }
+            }
+            require_once conf::$ROOT . 'system/etc/functions.php';
+
+            if ($modul == 'ajax') {
+                $controller_file = 'ajax_controller.php';
+                if (file_exists("system/controllers/" . $controller_file)) {
+                    include "system/controllers/" . $controller_file;
+                    $controller_name = 'ajax_controller';
+                    if (class_exists($controller_name)) {
+                        $controller = new $controller_name;
+                        $action = $path[1];
+                        if (method_exists($controller, $action)) {
+                            // вызываем действие контроллера
+                            $param = '';
+                            if (isset($param))
+                                $controller->$action($param);
+                            else
+                                $controller->$action();
+                        } else {
+                            //          			throw new Exception('action_not_found');
+                        }
+                    } else {
+                        throw New Exception('Controller_class_not_found');
+                    }
+                }
+            } else {
+                $fields = array();
+                $fields['filename'] = $modul . '_controller';
+
+                self::$fields = $fields;
+
+                //      if(isset($fields['filename'])){
+                $controller_file = $fields['filename'] . '.php';
+                if (file_exists("system/controllers/" . $controller_file)) {
+                    include "system/controllers/" . $controller_file;
+                    $controller_name = $fields['filename']; //$url;//'simple_page_tpl';
+                    if (class_exists($controller_name)) {
+                        $controller = new $controller_name;
+                        
+                        // если мы не администратор и главный контроллер, к которому мы обращаемся, 
+                        // это admin_cab - выдаем предупреждение
+                        if ($_SESSION["niiis"]["role"] != "administrator" && $path[0] == 'admin_cab') {
+//                            echo "<p align='center'>Необходимо авторизоваться как Администратор!</p>";
+//                            include "system/views/admin/admin_error_view.php";
+                        }
+                        
+                        $action = 'index';
+                        if (isset($path[1])) {
+                            $action = $path[1] . '';
+                        }
+                        
+                        if (method_exists($controller, $action)) {
+                            // вызываем действие контроллера
+                            $param = '';
+                            if (isset($param))
+                                $controller->$action($param);
+                            else
+                                $controller->$action();
+                        } else {
+                            //          			throw new Exception('action_not_found');
+                        }
+                    } else {
+                        throw New Exception('Controller_class_not_found');
+                    }
+                } else {
+                    throw New Exception('Controller_not_found');
+                }
+            }
+        } else {
+            $fields = array();
+
+            $fields['filename'] = 'login_controller';
+
+            self::$fields = $fields;
+
+//      if(isset($fields['filename'])){
+            $controller_file = $fields['filename'] . '.php';
+            if (file_exists("system/controllers/" . $controller_file)) {
+                include "system/controllers/" . $controller_file;
+                $controller_name = $fields['filename']; //$url;//'simple_page_tpl';
+                if (class_exists($controller_name)) {
+                    $controller = new $controller_name;
+                    //          $action = $path[1];
+                    $action = 'index';
+//                    if (isset($path[1])) {
+//                        $action = $path[1] . '';
+//                    }
+                    if (method_exists($controller, $action)) {
+                        // вызываем действие контроллера
+                        $param = '';
+                        if (isset($param))
                             $controller->$action($param);
                         else
                             $controller->$action();
+                    } else {
+                        //          			throw new Exception('action_not_found');
+                    }
+                } else {
+                    throw New Exception('Controller_class_not_found');
                 }
-                else
-                {
-  //          			throw new Exception('action_not_found');
-                }
-          }else{
-            throw New Exception('Controller_class_not_found');
-          }
-        }
-      }else{
-        $fields=array();
-        $fields['filename']=$modul.'_controller';
-
-        self::$fields=$fields;
-
-  //      if(isset($fields['filename'])){
-          $controller_file = $fields['filename'].'.php';
-          if(file_exists("system/controllers/".$controller_file)){
-            include "system/controllers/".$controller_file;
-            $controller_name=$fields['filename'];//$url;//'simple_page_tpl';
-            if (class_exists($controller_name)){
-              $controller = new $controller_name;
-              
-              $action = 'index';
-              if(isset($path[1])){
-                $action = $path[1].'';
-              }
-                  if (method_exists($controller, $action))
-                  {
-                    // вызываем действие контроллера
-                    $param='';
-                    if (isset($param))
-                        $controller->$action($param);
-                    else
-                        $controller->$action();
-                  }
-                  else
-                  {
-    //          			throw new Exception('action_not_found');
-                  }
-            }else{
-              throw New Exception('Controller_class_not_found');
+            } else {
+                throw New Exception('Controller_not_found');
             }
-          }else{
-            throw New Exception('Controller_not_found');
-          }
-      }
-    }else{
-      $fields=array();
-
-      $fields['filename']='login_controller';
-
-      self::$fields=$fields;
-
-//      if(isset($fields['filename'])){
-        $controller_file = $fields['filename'].'.php';
-        if(file_exists("system/controllers/".$controller_file)){
-          include "system/controllers/".$controller_file;
-          $controller_name=$fields['filename'];//$url;//'simple_page_tpl';
-          if (class_exists($controller_name)){
-            $controller = new $controller_name;
-  //          $action = $path[1];
-            $action = 'index';
-            if(isset($path[1])){
-                $action = $path[1].'';
-              }
-                if (method_exists($controller, $action))
-                {
-                  // вызываем действие контроллера
-                  $param='';
-                  if (isset($param))
-                      $controller->$action($param);
-                  else
-                      $controller->$action();
-                }
-                else
-                {
-  //          			throw new Exception('action_not_found');
-                }
-          }else{
-            throw New Exception('Controller_class_not_found');
-          }
-        }
-        else{
-          throw New Exception('Controller_not_found');
         }
     }
-		
-	
-	}
-    
+
 }
+
 ?>
