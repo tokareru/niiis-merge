e44ec0fd2cebe3c048036099ab938cdc1aa806c0
makss <makss@DESKTOP-LK19C8L>
2020-05-21 17:54:30 +0300

    fix reset drawing sizes

--- system/models/drawing_main_text_ajax_model.php
+++ system/models/drawing_main_text_ajax_model.php
@@ -93,7 +93,7 @@ class drawing_main_text_ajax_model extends model {
     }
     function save_size_and_positions(){
         if($_SERVER["REQUEST_METHOD"]=="POST"){
-            $sql = "UPDATE DRAWING_SIZE 
+            $sql = "UPDATE DRAWING_SIZE SET
                           size_1 = :size_1,
                           size_2 = :size_2,
                           size_3 = :size_3,
